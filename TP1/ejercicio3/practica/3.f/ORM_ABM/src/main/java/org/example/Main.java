8f091b4c7b9cbdbc6da88aef5d6f006d4cf4a40b
beckLic <augustobeck472@gmail.com>
2026-08-14 10:51:14 -0300

    configurado Spring Boot, Lombok, JPA con Hibernate y H2

--- TP1/ejercicio3/practica/3.f/ORM_ABM/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Persistence;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
