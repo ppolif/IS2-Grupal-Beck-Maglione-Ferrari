d9557e82a2c10a6a172685719fb2af782f69da0e
Martin Maglione <maglione.martin@uncuyo.edu.ar>
2026-08-20 15:06:44 -0300

    guia de bootstrap terminada

--- TP1/ejercicio4/practica/3.d/lib/pages/iniciar-sesion.js
+++ TP1/ejercicio4/practica/3.d/lib/pages/iniciar-sesion.js
@@ -1,17 +1,38 @@
+import { login } from '../service/user.js'
+
 const form = document.getElementById("form")
 const userInput = document.getElementById("user")
 const passwordInput = document.getElementById("password")
 
-form.addEventListener('submit',(e)=>{
+//TOAST
+const _successToast = document.getElementById('successToast')
+const _errorToast = document.getElementById('errorToast')
+
+form.addEventListener('submit',async(e)=>{
     e.preventDefault()
+
+    const successToast = bootstrap.Toast.getOrCreateInstance(_successToast)
+    const errorToast = bootstrap.Toast.getOrCreateInstance(_errorToast)
 
     const username = userInput.value
     const password = passwordInput.value
 
+    // validaciones
     if (username === '' || password === ''){
-        alert('Completa todos los campos')
+        errorToast.show()
         return
     }
 
-    
+    // Si es valido
+    const res = await login({username,password})
+
+    if (res.token){
+        successToast.show()
+
+        setTimeout(()=>{
+            window.location.href = '/';
+        }, 1000)
+    } else{
+        errorToast.show()
+    }
 })
